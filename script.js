86a5ddd47f8566d449d35cb7c00a89f14fea86ee
Steven Chen <steven.chen.121@gmail.com>
2022-06-07 22:54:08 -0400

    Added a somewhat random initial position so that it doesn't take the exact same path everytime.

--- script.js
+++ script.js
@@ -3,6 +3,28 @@ const dvd = document.querySelector('.logo-container');
 const step = 4;
 let xFactor = 1;
 let yFactor = 1;
+let winWidth = window.innerWidth;
+let winHeight = window.innerHeight;
+
+const init = () => {
+
+  window.addEventListener('resize', () => {
+    winWidth = window.innerWidth;
+    winHeight = window.innerHeight;
+  });
+
+  const initX = randCoord(winWidth / 2);
+  const initY = randCoord(winHeight / 2);
+
+  dvd.style.left = initX + 'px';
+  dvd.style.top = initY + 'px';
+
+  const moveInterval = setInterval(dvdBounce, 25);
+};
+
+const randCoord = (max) => {
+  return Math.floor(Math.random() * max);
+};
 
 const dvdBounce = () => {
   const coords = dvd.getBoundingClientRect();
@@ -18,4 +40,4 @@ const dvdBounce = () => {
   dvd.style.top = (coords.top + step * yFactor) + 'px';
 };
 
-const moveInterval = setInterval(dvdBounce, 25);
+init();
